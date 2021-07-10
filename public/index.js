b3351c4d15b4f77d653ad91c0b5697e6f551f57d
Blakthorne <dlpolar38@gmail.com>
2021-07-10 12:17:23 -0400

    change footer to flex

--- public/index.js
+++ public/index.js
@@ -3,5 +3,11 @@ document.getElementById("header-nav").innerHTML =
  '<a href="/programmer/">Programmer</a>' + ' • ' +
  '<a href="/learner/">Learner</a>';
 
+ document.getElementById("header-nav-mobile").innerHTML =
+ '<a href="/">Home</a>' + ' <br> ' +
+ '<a href="/musician/">Musician</a>' + ' • ' +
+ '<a href="/programmer/">Programmer</a>' + ' • ' +
+ '<a href="/learner/">Learner</a>';
+
 //  document.getElementById("footer").innerHTML =
 //  '<p>dlpolar38@gmail.com</p>';
