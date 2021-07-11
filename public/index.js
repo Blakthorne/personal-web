5df8c13eab34fa47d12d340955bbbd048ebf0bd8
Blakthorne <dlpolar38@gmail.com>
2021-07-11 00:57:00 -0400

    home page content updated

--- public/index.js
+++ public/index.js
@@ -9,5 +9,12 @@ document.getElementById("header-nav").innerHTML =
  '<a href="/programmer/">Programmer</a>' + ' • ' +
  '<a href="/learner/">Learner</a>';
 
-//  document.getElementById("footer").innerHTML =
-//  '<p>dlpolar38@gmail.com</p>';
+ document.getElementById("footer").innerHTML =
+ '<a class="email" href="mailto:dlpolar38@gmail.com">dlpolar38@gmail.com</a>' +
+ '<div class="break"></div>' +
+ '<a class="in-icon" href="https://www.linkedin.com/in/davidpolar789/" target="_blank" rel="noopener noreferrer">' +
+    '<img src="/images/in.png">' +
+ '</a>' +
+ '<a class="github-icon" href="https://github.com/Blakthorne" target="_blank" rel="noopener noreferrer">' +
+    '<img src="/images/github.png">' +
+ '</a>';
