79422ac1636fa3c993ab4250331fbb7324963165
Blakthorne <dlpolar38@gmail.com>
2025-07-02 23:03:49 -0400

    reorganize project to use at Vercel

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 document.getElementById("header").innerHTML =
 '<a href="/">' +
-      '<img id="logo" src="/public/images/icon.svg" alt="icon">' +
+      '<img id="logo" src="/images/icon.svg" alt="icon">' +
 '</a>' +
 '<nav id="header-nav">' +
 '<a href="/about-me" id="about-me-nav">About Me</a>' +
@@ -11,11 +11,11 @@ document.getElementById("footer").innerHTML =
 '<a class="email" href="mailto:dlpolar38@gmail.com">dlpolar38@gmail.com</a>' +
 '<div class="break"></div>' +
 '<a class="in-icon" href="https://www.linkedin.com/in/davidpolar789/" target="_blank" rel="noopener noreferrer">' +
-   '<img src="/public/images/in.png">' +
+   '<img src="/images/in.png">' +
 '</a>' +
 '<a class="github-icon" href="https://github.com/Blakthorne" target="_blank" rel="noopener noreferrer">' +
-   '<img src="/public/images/github.png">' +
+   '<img src="/images/github.png">' +
 '</a>' +
 '<a class="instagram-icon" href="https://www.instagram.com/daves.trombone/" target="_blank" rel="noopener noreferrer">' +
-   '<img src="/public/images/instagram.svg">' +
+   '<img src="/images/instagram.svg">' +
 '</a>';
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/about-me', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/about-me.html'));
+});
+
+app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/portfolio.html'));
+});
+
+module.exports = app;
