a7f0ff56a26f88b6db132ecb79933a4434926cb7
Blakthorne <dlpolar38@gmail.com>
2025-08-18 23:54:10 -0400

    added service-integrator section in development portfolio

--- server.js
+++ server.js
@@ -17,4 +17,9 @@ app.get('/portfolio', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/portfolio.html'));
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
 module.exports = app;
